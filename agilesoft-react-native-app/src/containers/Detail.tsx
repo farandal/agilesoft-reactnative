4caca1b88074881e40033cc12da8a6c6163dcf39
Francisco Aranda <farandal@gmail.com>
2021-08-21 16:30:39 -0400

    Added initial components and containers for Splash, Home, Detail, and extra Settings

---
+++ agilesoft-react-native-app/src/containers/Detail.tsx
@@ -0,0 +1,41 @@
+import React, {memo} from 'react';
+import {useTranslation} from 'react-i18next';
+import {ActivityIndicator, Button, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {useTheme} from '../Theme';
+import {fetchUserAsync} from '../actions/usersActions';
+import changeThemeAction from '../actions/themeActions';
+import Colors from '../constants/colors';
+import useSelector from '../utils/useSelector';
+import { ThemeState } from 'src/reducers/themeReducer';
+
+
+function Detail() {
+  const { Common, Fonts, Gutters, Layout } = useTheme()
+
+  const user = useSelector(state => state.users.user);
+  const {t} = useTranslation();
+  const dispatch = useDispatch();
+  const fetchUser = () => {
+    const userId = '1';
+    dispatch(fetchUserAsync.request(userId));
+  };
+
+  const instructions = Platform.select({
+    ios: t('iosInstruction'),
+    android: t('androidInstruction'),
+  });
+
+  const changeTheme = ({ theme, darkMode }: Partial<ThemeState>) => {
+    dispatch(changeThemeAction({ theme, darkMode }))
+  }
+
+  return (
+    <View style={[Layout.fill, Layout.colCenter]}>
+      <Text>Detail</Text>
+
+    </View>
+  );
+}
+
+export default memo(Detail);
