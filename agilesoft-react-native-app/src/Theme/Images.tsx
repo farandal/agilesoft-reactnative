714a4dea2f3bd91c833da80481094abd1c847689
Francisco Aranda <farandal@gmail.com>
2021-08-21 22:01:00 -0400

    Tema

---
+++ agilesoft-react-native-app/src/Theme/Images.tsx
@@ -0,0 +1,12 @@
+import {ThemeImages, ThemeVariables} from './theme.type';
+
+/**
+ *
+ * @param Theme can be spread like {Colors, NavigationColors, Gutters, Layout, Common, ...args}
+ * @return {*}
+ */
+export default function ({}: ThemeVariables): ThemeImages {
+  return {
+    logo: require('../assets/images/logo.png'),
+  };
+}
